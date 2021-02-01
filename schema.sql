d4c512d6eaa90ef71dabe5e7e66a5791e4705681
John Manack <jmnck28@gmail.com>
2021-02-01 18:19:27 -0500

    intial commit

---
+++ schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name varchar(50) not null,
+    email varchar(50) not null
+)
---
+++ seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (name, email) VALUES
+('Alice', 'alice@email.com'),
+('Bob', 'bob@email.com'),
+('Cho', 'cho@email.com'),
+('Daryl', 'daryl@email.com'),
+('Emmy', 'emmy@email.com');
